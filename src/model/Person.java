db58f59c85c1cab1c788cb4791792b922ded8ea7
njse22 <njse22@gmail.com>
2025-01-27 11:20:56 -0500

    upload changes

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,17 +1,24 @@
 package model; 
 
+import java.lang.Math; 
+
 public class Person {
 
   // Declarar una variable
+  // Atributo de la Clase
   private int age;
   private String name;
+  private double weight; 
+  private double height; 
   
   // método (Función) -> Especial -> Constructor 
-  public Person(int age1, String name1){
+  public Person(int age1, String name1, double weight, double height){
     // inicializar las variables de la clase 
     // Atributos de una Clase 
     this.age = age1; 
     this.name = name1; 
+    this.weight = weight;
+    this.height = height; 
     
   }
   
@@ -20,5 +27,13 @@ public class Person {
     return this.name;
   }
 
+  public String getImc(){
+    String msj = "El IMC de la persona es: " + (weight/Math.pow(height, 2));
+    return msj; 
+  }
+  
+  public String getImc(double w, double h){
+    return "IMC: " + (w / Math.pow(h,2) );
+  }
   
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,21 +1,35 @@
 package ui; // Paquete de la clase -> la carpeta donde se guarda el archivo 
 
-import model.Person; 
+import model.Person; // Importar clase de otro paquete
+import java.util.Scanner;
+
 
 public class Main{ // Nombre de la clase -> el nombre del archivo 
 
   // Método -> Método pirncipal del lenguaje -> Método main
   public static void main(String[] args){
   
+    Scanner reader = new Scanner(System.in);
+    System.out.println("Type a number: "); 
+    //int number1 = reader.nextInt();
+    
+    
+    //System.out.println(number1);
+  
     // Declaración e inicialización de la variable
+    // Tipo de dato nombreVariable = valor;
     int a = 10;
     System.out.println(a); 
     
-    Person persona1 = new Person(20, "Persona1"); 
+    // Tipo de dato (Clase) | NombreVariable = new -> (Llamado al método constructor de la clase) 
+    Person persona1 = new Person(20, "Persona1", 60, 1.60); 
     
     String nombre = persona1.getName();
     
     System.out.println(nombre);
+    
+    System.out.println(persona1.getImc());
+    System.out.println(persona1.getImc(50, 1.70)); 
   
   } // Cierro el método main
   
