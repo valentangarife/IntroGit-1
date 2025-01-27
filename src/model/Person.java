6567c271181e5b76dd472ea54fbde83d83efe4a1
njse22 <njse22@gmail.com>
2025-01-27 09:24:46 -0500

    Upload Constructor of Person Class

--- src/model/Person.java
+++ src/model/Person.java
@@ -4,6 +4,16 @@ public class Person {
 
   // Declarar una variable
   private int age;
+  private String name;
+  
+  // método (Función) -> Especial -> Constructor 
+  public Person(int age1, String name1){
+    // inicializar las variables de la clase 
+    // Atributos de una Clase 
+    this.age = age1; 
+    this.name = name1; 
+    
+  }
 
   
 }
